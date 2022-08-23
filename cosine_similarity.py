22cc1f86b7ff6bf61f7943b804a0fc6159af0350
Yaromir <yaromirkap@gmail.com>
2022-08-23 22:47:11 +0300

    cosine_similarity.py

---
+++ cosine_similarity.py
@@ -0,0 +1,10 @@
+# Напишите функцию, которая принимает на вход два вектора и рассчитывает косинусное расстояние данных векторов.
+
+import numpy as np
+from numpy.linalg import norm
+
+A = np.array([192, 168])
+B = np.array([0, 1])
+
+cosine = np.dot(A, B)/(norm(A) * norm(B))
+print("Cosine Similarity:", cosine)
