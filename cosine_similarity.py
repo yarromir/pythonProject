f76936eb8addb6437eca829721ffc53fda580c5e
Yaromir <yaromirkap@gmail.com>
2022-08-27 13:23:53 +0300

    cosine similarity by numpy/scipy

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -4,12 +4,19 @@ import numpy as np
 from numpy.linalg import norm
 from scipy.spatial import distance
 
-A = np.array([192, 168])
-B = np.array([0, 1])
+a = np.array([192, 168])
+b = np.array([0, 1])
 
-cosine_numpy = 1 - np.dot(A, B)/(norm(A) * norm(B))
-print(cosine_numpy)
 
-cosine_scipy = distance.cosine(A, B)
-print(cosine_scipy)
+def cosine_similarity_numpy(a, b):
+    cosine_numpy = 1 - np.dot(a, b) / (norm(a) * norm(b))
+    return cosine_numpy
 
+
+def cosine_similarity_scipy(a, b):
+    cosine_scipy = distance.cosine(a, b)
+    return cosine_scipy
+
+
+print(cosine_similarity_numpy(a, b))
+print(cosine_similarity_scipy(a, b))
